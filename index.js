c349ed8bb6e32b4416fd67e2c9ba176764d4f861
Alexey <peretsmobil@gmail.com>
2019-07-18 15:43:26 +0300

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+const { fetchGasPrice, isValidProof } = require('./utils')
+const { numberToHex, toWei, toHex } = require('web3-utils')
+
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const { netId, rpcUrl, privateKey, mixerAddress, defaultGasPrice } = require('./env.json')
+
+const Web3 = require('web3')
+const web3 = new Web3(rpcUrl, null, { transactionConfirmationBlocks: 1 })
+const account = web3.eth.accounts.privateKeyToAccount('0x' + privateKey)
+web3.eth.accounts.wallet.add('0x' + privateKey)
+web3.eth.defaultAccount = account.address
+
+const mixerABI = require('./mixerABI.json')
+const mixer = new web3.eth.Contract(mixerABI, mixerAddress)
+const gasPrices = { fast: defaultGasPrice }
+
+app.get('/', function (req, res) {
+  // just for testing purposes
+  res.send(`Tornado mixer relayer. Gas Price is ${JSON.stringify(gasPrices)}`)
+})
+
+app.post('/relay', async (req, resp) => {
+  const { valid , reason } = isValidProof(req.body)
+  if (!valid) {
+    console.log('Proof is invalid:', reason)
+    return resp.status(400).send('Proof is invalid')
+  }
+
+  let { pi_a, pi_b, pi_c, publicSignals } = req.body
+
+  // TODO
+  // if (bigInt(proof.publicSignals[3]) < getMinimumFee()) {
+  //   resp.status(403).send('Fee is too low')
+  // }
+
+  try {
+    const nullifier = publicSignals[1]
+    const isSpent = await mixer.methods.isSpent(nullifier).call()
+    if (isSpent) {
+      throw new Error('The note has been spent')
+    }
+    const gas = await mixer.methods.withdraw(pi_a, pi_b, pi_c, publicSignals).estimateGas()
+    const result = mixer.methods.withdraw(pi_a, pi_b, pi_c, publicSignals).send({
+      gas: numberToHex(gas + 50000),
+      gasPrice: toHex(toWei(gasPrices.fast.toString(), 'gwei')),
+      // TODO: nonce
+    })
+    result.once('transactionHash', function(hash){
+      resp.send({ transaction: hash })
+    }).on('error', function(e){
+      console.log(e)
+      resp.status(400).send('Proof is malformed')
+    })
+  } catch (e) {
+    console.log(e)
+    resp.status(400).send('Proof is malformed or spent')
+  }
+})
+
+app.listen(8000)
+
+if (netId === 1) {
+  fetchGasPrice({ gasPrices })
+  console.log('Gas price oracle started.')
+}
---
+++ utils.js
@@ -0,0 +1,81 @@
+const fetch = require('node-fetch')
+const { isHexStrict } = require('web3-utils')
+const { gasOracleUrls } = require('./env.json')
+
+async function fetchGasPrice({ gasPrices, oracleIndex = 0 }) {
+  oracleIndex = (oracleIndex + 1) % gasOracleUrls.length
+  try {
+    const response = await fetch(gasOracleUrls[oracleIndex])
+    if (response.status === 200) {
+      const json = await response.json()
+
+      if (json.slow) {
+        gasPrices.low = Number(json.slow)
+      }
+      if (json.safeLow) {
+        gasPrices.low = Number(json.safeLow)
+      }
+      if (json.standard) {
+        gasPrices.standard = Number(json.standard)
+      }
+      if (json.fast) {
+        gasPrices.fast = Number(json.fast)
+      }
+    } else {
+      throw Error('Fetch gasPrice failed')
+    }
+    setTimeout(() => fetchGasPrice({ gasPrices, oracleIndex }), 15000)
+  } catch (e) {
+    setTimeout(() => fetchGasPrice({ gasPrices, oracleIndex }), 15000)
+  }
+}
+
+function isValidProof(proof) {
+  // validator expects `websnarkUtils.toSolidityInput(proof)` output
+
+  if (!(proof.pi_a && proof.pi_b && proof.pi_c && proof.publicSignals)) {
+    return { valid: false, reason: 'One of inputs is empty. There must be pi_a, pi_b, pi_c and publicSignals' }
+  }
+
+  Object.keys(proof).forEach(key => {
+    if (!Array.isArray(proof[key])) {
+      return { valid: false, reason: `Corrupted ${key}` }
+    }
+    if (key === 'pi_b') {
+      if (!Array.isArray(proof[key][0]) || !Array.isArray(proof[key][1])) {
+        return { valid: false, reason: `Corrupted ${key}` }
+      }
+    }
+  })
+
+  if (proof.pi_a.length !== 2) {
+    return { valid: false, reason: 'Corrupted pi_a' }
+  }
+
+  if (proof.pi_b.length !== 2 || proof.pi_b[0].length !== 2 || proof.pi_b[1].length !== 2) {
+    return { valid: false, reason: 'Corrupted pi_b' }
+  }
+
+  if (proof.pi_c.length !== 2) {
+    return { valid: false, reason: 'Corrupted pi_c' }
+  }
+
+  if (proof.publicSignals.length !== 4) {
+    return { valid: false, reason: 'Corrupted publicSignals' }
+  }
+
+  for (let [key, input] of Object.entries(proof)) {
+    if (key === 'pi_b') {
+      input = input[0].concat(input[1])
+    }
+
+    for (let i = 0; i < input.length; i++ ) {
+      if (!isHexStrict(input[i]) || input[i].length !== 66) {
+        return { valid: false, reason: `Corrupted ${key}` }
+      }
+    }
+  }
+  return { valid: true }
+}
+
+module.exports = { fetchGasPrice, isValidProof }
