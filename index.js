7a14828e0c2b3306af5b8114a8d1149b97acf668
poma <Semenov.Roman@mail.ru>
2019-07-18 21:15:59 +0300

    code style

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 const { netId, rpcUrl, privateKey, mixerAddress, defaultGasPrice } = require('./config')
 const { fetchGasPrice, isValidProof } = require('./utils')
