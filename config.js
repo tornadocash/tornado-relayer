dece7a0ced619ed30d69da44f82ee697b797d81e
Roman Storm <roman@peppersec.com>
2020-05-08 12:08:59 -0700

    bump version

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-  version: 2.4,
+  version: 2.5,
   netId: Number(process.env.NET_ID) || 42,
   redisUrl: process.env.REDIS_URL,
   rpcUrl: process.env.RPC_URL || 'https://kovan.infura.io/',
