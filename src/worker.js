66082a8fbc9d5670de9733e143a22ea2b2bf3285
Danil Kovtonyuk <danx.kov@gmail.com>
2021-03-10 04:13:51 +1000

    increase gas limit

--- src/worker.js
+++ src/worker.js
@@ -211,6 +211,7 @@ async function getTxObject({ data }) {
       value: data.args[5],
       to: contract._address,
       data: calldata,
+      gasLimit: gasLimits[data.type],
     }
   } else {
     const method = data.type === jobType.MINING_REWARD ? 'reward' : 'withdraw'
@@ -218,6 +219,7 @@ async function getTxObject({ data }) {
     return {
       to: minerContract._address,
       data: calldata,
+      gasLimit: gasLimits[data.type],
     }
   }
 }
